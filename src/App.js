0f45ec931979c4309f3394e9f25656a709948276
spandana <salla.spandana@gmail.com>
2024-10-06 09:04:36 +0530

    Restored main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import FunctionalGreet from './components/FunctionalGreet';
-import UseEffect from './components/hooks/UseEffect';
-import UseEffectConditionally from './components/hooks/useEffectConditionally';
-import UseStateHook from './components/hooks/UseStateHook';
-import UseStateWithArray from './components/hooks/UseStatewithArray';
-import UseStateWithObjects from './components/hooks/UseStateWithObjects';
-import UseStateWithPrevState from './components/hooks/UseStateWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      {/* <FunctionalGreet name="Bommanas">Here is the list of Family Members</FunctionalGreet> */}
-      {/* <FunctionalGreet name="Sravan"/>
-      <FunctionalGreet name="Spandana"/>
-      <FunctionalGreet name="Ushika"/> */}
-      {/* <UseStateHook />
-      <UseStateWithPrevState />
-      <UseStateWithObjects />
-      <UseStateWithArray /> */}
-      <UseEffect />
-      <UseEffectConditionally />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
